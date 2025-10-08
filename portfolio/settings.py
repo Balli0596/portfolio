e605a3746a887ea180063a32cf07fa01cf8af937
Balli0596 <maharanabali90@gmail.com>
2025-10-08 15:55:58 +0530

    Add templates folder

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': [BASE_DIR / 'templates'],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
